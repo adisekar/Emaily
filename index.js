17457478c0f5b4fa0c33d16e516f70dd26244f8f
Adi <adithya.sekar@ibm.com>
2020-11-12 20:57:33 -0500

    Updated message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ hi: 'hello world' });
 });
 
 // Gte Port from Heroku, or use local
